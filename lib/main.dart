97cff7f666eae2226e0287d706e12aa888bb5b9b
atul2903 <atulswarnkar29@gmail.com>
2023-04-04 13:23:28 +0530

    my initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../firebase_options.dart';
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage() ,
+    ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Home page')),
+      ),
+      body: FutureBuilder(
+        
+        future: Firebase.initializeApp(
+         options: DefaultFirebaseOptions.currentPlatform,
+       ),
+
+        builder: (context, snapshot) {
+          switch(snapshot.connectionState){
+            
+            case ConnectionState.done:
+             final user=FirebaseAuth.instance.currentUser;
+             if(user?.emailVerified??false){
+              print('your email is verified');
+             }
+             else{
+              print("your email isn't verified");
+             }
+             return Text('done');
+              
+        default:
+         return const Text('Loading....');
+          }
+         
+        },
+        
+      ),
+    );
+  }
+}
+
